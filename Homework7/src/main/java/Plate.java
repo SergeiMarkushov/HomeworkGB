23c350f037ab99ace7196da3805e657627be921f
Sergei Markushov <s.markushov@icloud.com>
2022-04-01 11:38:28 +0300

    Добавил условие к уменьшению еды. Исправил грамматические ошибки

--- Homework7/src/main/java/Plate.java
+++ Homework7/src/main/java/Plate.java
@@ -6,17 +6,17 @@ public class Plate {
     }
     public void info(){
         if(getFood() !=0) {
-            System.out.println("Food in plate: " + getFood());
+            System.out.println("Food in the plate: " + getFood());
         }else{
-            System.out.println("Place food in plate or don't eat");
+            System.out.println("Place food in the plate or don't eat");
         }
     }
 
     public void decreaseFood(int appetite){
-        if (getFood() !=0) {
+        if (getFood() !=0 && appetite <= this.food) {
             this.food -= appetite;
         }else{
-            System.out.println("No food in the plate");
+            System.out.println("No food in the plate or appetite of the cat is more then food in the plate");
         }
     }
     public void increaseFood(int puttFood){
